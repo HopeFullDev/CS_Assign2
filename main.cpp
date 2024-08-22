3a4ca0ae42dc498013633571a9982ff1c634d563
Lalit Kumar <2024csm1011@iitrpr.ac.in>
2024-08-22 20:59:44 +0530

    Added more boilerplate

--- main.cpp
+++ main.cpp
@@ -2,15 +2,22 @@
 // #include <unordered_map>
 // #include <vector>
 #include "bits/stdc++.h"
+#include "config.h"
 
+#define ull unsigned long long
 class Task {
 public:
   unsigned long long task_id;
-  std::unordered_map<unsigned long long int, unsigned long long int>
+  std::unordered_map<unsigned long long, unsigned long long>
       PageTableImplementationA;
+
+  void requestMemory(unsigned long long size,
+                     unsigned long long virtual_address);
 };
 
 class MemoryManager {
 public:
-  unsigned long long freeMemory = 0x00000000000;
+  unsigned long long freeMemory = PHYSICAL_MEM_SIZE;
+  unsigned long long frameSize = 0x000000000;
+  void assignMemory(unsigned long long size, unsigned long long task_id);
 };
