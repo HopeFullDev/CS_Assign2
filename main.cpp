222f9aef9b8d1c570008ca7ae11be26069816b4d
Lalit Kumar <2024csm1011@iitrpr.ac.in>
2024-08-22 21:48:31 +0530

    Added commments for task class

--- main.cpp
+++ main.cpp
@@ -4,13 +4,41 @@
 #include "bits/stdc++.h"
 #include "config.h"
 
+// USING unsigned long long for all numbers
 #define ull unsigned long long
+
+// Table for keeping track of how much memory was given to which task
+class MM_table {
+public:
+  //  task id of the task which requested the memory
+  ull task_id,
+      // size of memory requested by the task
+      size,
+      // first frame no alloted to the task
+      firstAllotedFrameNo,
+      // it is possible that the size requested by the task is greater than the
+      // page size then multiple frames will be alloted that record is kept here
+      noOfFramesAlloted;
+};
+
+// this class will define the three implementation of page tables and the
+// associated variables to track the allocation of memory
 class Task {
 public:
+  //  each task will have some id attached to it that is stored in this variable
   unsigned long long task_id;
+  //  the size of page table
+  ull pageTableSize = PAGE_SIZE;
+  // no of pages in the page table these page no will be mapped to frame no in
+  // physical memory
+  ull noOfPages = VIRTUAL_MEM_SIZE / pageTableSize;
+  // this is the first implementation of page table as key value in a hash map
   std::unordered_map<unsigned long long, unsigned long long>
       PageTableImplementationA;
-
+  // this function will request the memory which is frame no from the memory
+  // manager , and in return it will get the first alloted frame no in case of
+  // requested size is larger than frame size we will have to handle that in
+  // this as well
   void requestMemory(unsigned long long size,
                      unsigned long long virtual_address);
 };
@@ -18,6 +46,11 @@ public:
 class MemoryManager {
 public:
   unsigned long long freeMemory = PHYSICAL_MEM_SIZE;
-  unsigned long long frameSize = 0x000000000;
-  void assignMemory(unsigned long long size, unsigned long long task_id);
+  unsigned long long frameSize = PAGE_SIZE;
+  ull nextFreeFrame = 0x000000000;
+  ull noOfFreeFrames = PHYSICAL_MEM_SIZE / PAGE_SIZE;
+  std::vector<MM_table> allotedMemoryTable;
+  ull assignMemory(unsigned long long size, unsigned long long task_id);
 };
+
+static MemoryManager mmInstance;
